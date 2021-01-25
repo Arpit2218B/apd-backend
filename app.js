cd168035df3e573a4fd65a8a077c88afa0e18d53
Arpit Rathi <galjonit2218@gmail.com>
2021-01-25 12:38:17 +0530

    added cors handler via npm package

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const mongoConnect = require('./utils/database').dbConnect;
 const tasksRouter = require('./routes/tasks');
@@ -10,6 +11,8 @@ const authMiddleWare = require('./middlewares/auth');
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use(cors());
 
 app.use('/tasks', authMiddleWare, tasksRouter);
 
